334afb1de10db66d5eb30643e1675c85d7a3d8ba
anusha15160819 <anupatibandla0208@gmail.com>
2024-12-11 21:41:41 +0530

    added some files

---
+++ src/Practice1/Jobs.java
@@ -0,0 +1,15 @@
+package Practice1;
+
+public class Jobs {
+
+	public void Subject() {
+		System.out.println("Subject first job Next");
+		System.out.println("Subject first job Next");
+                System.out.println("Subject first job Next");
+		System.out.println("Subject first job Next");
+                System.out.println("Subject first job Next sdsadsad");
+                System.out.println("Subject first job Next sddsadsa");
+
+
+	}
+}
